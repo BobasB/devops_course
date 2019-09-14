5bdf7a6ca4f52ef34f54d7c2fc005956e9267a0a
Bohdan Buhyl <bbuhyl@cisco.com>
2019-09-14 20:20:34 +0300

    Admin - example app for Lab 2

---
+++ lab2/Makefile
@@ -0,0 +1,19 @@
+.DEFAULT_GOAL := all
+
+all: install test run deploy
+
+install:
+	@echo "Installing pipenv and dependencies."
+	# Ваш код тут
+
+test:
+	@echo "Start tests."
+	# Ваш код тут
+
+run:
+	@echo "Run Python app."
+	# Ваш код тут
+
+deploy:
+	@echo "Adding and Committing results.txt to git."
+	# Ваш код тут
--- lab2/app.py
+++ lab2/app.py
@@ -1,17 +1,31 @@
 import requests
 
 
-def main(url):
+def main(url=''):
+    if not url:
+        print("No URL passed to function")
+        return False
     r = requests.get(url=url)
     d = r.json()
-    if "date" in d.keys():
-        print(d['date'])
-
     if "time" in d.keys():
-        print(d['time'])
+        print("Time is: ", d['time'])
+    try:
+        print("Date is: ", d['date'])
+    except KeyError:
+        print("No date in response!!!")
+        raise KeyError
 
     return True
 
 
+def home_work():
+    # Ваш захист
+    pass
+
+
 if __name__ == "__main__":
+    a = "="*40
+    print(a + "\nРезультат без параметрів: ")
+    main()
+    print(a + "\nРезультат з правильною URL: ")
     main('http://date.jsontest.com/')
