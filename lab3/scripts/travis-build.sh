f1651ac8f2cf331b7ac045616dd29d6e01773113
Bohdan Buhyl <bbuhyl@cisco.com>
2019-11-14 09:57:01 +0200

    admin: try with bash script 2

--- lab3/scripts/travis-build.sh
+++ lab3/scripts/travis-build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -ev
-pipenv run server > ./ci-build.log 2>&1 &
-pipenv run python monitoring.py
+pipenv run server > ./ci-build.log 2>&1 &>/dev/null && pipenv run python monitoring.py
 exit 0
